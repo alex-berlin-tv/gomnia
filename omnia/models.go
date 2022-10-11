a10d265cb9de9f41fbe6562d75e2cbc36a83e3e4
72nd <msg@frg72.com>
2022-10-11 12:23:51 +0200

    Response structs documented

--- omnia/models.go
+++ omnia/models.go
@@ -90,26 +90,42 @@ type QueryParameters struct {
 	AddStatistics Bool `url:"addStatistics,omitempty"`
 }
 
+// Metadata part of an API response.
 type ResponseMetadata struct {
-	Status         int     `json:"status"`
-	ApiVersion     string  `json:"apiversion,omitempty"`
-	Verb           string  `json:"verb"`
+	// The HTTP Status for this Call.
+	Status int `json:"status"`
+	// Version of the API.
+	ApiVersion string `json:"apiversion,omitempty"`
+	// The used HTTP Verb.
+	Verb string `json:"verb"`
+	// Internal Duration, needed to create the response.
 	ProcessingTime float64 `json:"processingtime"`
-	CalledWith     *string `json:"calledwith,omitempty"`
-	CalledFor      *string `json:"calledfor,omitempty"`
-	ForDomain      *string `json:"fordomain,omitempty"`
-	FromStage      *int    `json:"fromstage,omitempty"`
-	Notice         *string `json:"notice"`
-	ErrorHint      *string `json:"errorhint,omitempty"`
+	// The called Endoint and Parameter.
+	CalledWith *string `json:"calledwith,omitempty"`
+	// The `cfo` Parameter from the API Call.
+	CalledFor *string `json:"calledfor,omitempty"`
+	// The calling Domain ID.
+	ForDomain *string `json:"fordomain,omitempty"`
+	// The result was created by a Stage or Productive Server.
+	FromStage *int `json:"fromstage,omitempty"`
+	// If the Call uses deprecated Functionality, find here a Hint, what Attributes
+	// should be changed.
+	Notice *string `json:"notice"`
+	// If the Call failed, a Hint for the Failure Reason.
+	ErrorHint *string `json:"errorhint,omitempty"`
 }
 
 func (m ResponseMetadata) toMap() map[string]interface{} {
 	return structToMap(m)
 }
 
+// Information on the paging of an result.
 type ResponsePaging struct {
-	Start       int `json:"start"`
-	Limit       int `json:"limit"`
+	// The Start of the Query Range.
+	Start int `json:"start"`
+	// The given maximal Item List Length.
+	Limit int `json:"limit"`
+	// The maximally available Number of Items.
 	ResultCount int `json:"resultcount"`
 }
 
@@ -117,10 +133,16 @@ func (p ResponsePaging) toMap() map[string]interface{} {
 	return structToMap(p)
 }
 
+// The response of an nexxOmnia API call. As documented [here].
+//
+// [here]: https://api.docs.nexx.cloud/api-design/response-object
 type Response struct {
+	// Metadata.
 	Metadata ResponseMetadata `json:"metadata"`
-	Result   interface{}      `json:"result"`
-	Paging   *ResponsePaging  `json:"paging"`
+	// Acutal result of the call. Structure can vary widely.
+	Result interface{} `json:"result"`
+	// Optional information on the paging.
+	Paging *ResponsePaging `json:"paging"`
 }
 
 func structToMap(data interface{}) map[string]interface{} {
